bb1644ff7917e74855378f92ff538fe83ac047b4
vishalgandam <38121892+vishalgandam@users.noreply.github.com>
2018-04-06 01:19:27 -0500

    Microservices

---
+++ MicroServices/MicroServices-master/src/main/java/com/nagesh/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nagesh.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Microsevice1Application.class);
+	}
+
+}
